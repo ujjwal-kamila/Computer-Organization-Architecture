619d389b2a7c66264d20d7492c20e0cc6bdab868
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-10 22:48:58 +0530

    Full Adder code  using Verilog

--- Verilog/Full_add_new.v
+++ Verilog/Full_add_new.v
@@ -49,3 +49,79 @@ module tb_four_bit_full_adder;
     end
 
 endmodule
+
+//another best one
+
+/*
+module full_adder(
+    input a,b,cin,
+    output sum,carry);
+
+assign sum = a ^ b ^ cin;
+assign carry = (a & b)|(b & cin)|(cin & a);
+                
+endmodule
+
+module rca(
+    input [3:0]a,b,
+    input cin,
+    output [3:0]sum,
+    output c4);
+
+wire c1,c2,c3;      
+
+full_adder fa0(a[0],b[0],cin,sum[0],c1);
+full_adder fa1(a[1],b[1],c1,sum[1],c2);
+full_adder fa2(a[2],b[2],c2,sum[2],c3);
+full_adder fa3(a[3],b[3],c3,sum[3],c4);
+                
+endmodule
+
+
+module rca_tb;
+reg [3:0]a,b;
+reg cin;
+wire [3:0]sum;
+wire c4;
+
+rca dut(a,b,cin,sum,c4);
+
+initial begin
+
+$dumpfile("rca.vcd");
+$dumpvars(0,rca_tb);
+$monitor($time,,,  "a = %b, b = %b, cin = %b, sum = %b, c4 = %b", a, b, cin, sum, c4);
+            
+cin = 0;
+a = 4'b0110;
+b = 4'b1100;
+#10
+a = 4'b1110;
+b = 4'b1000;
+#10
+a = 4'b0111;
+b = 4'b1110;
+#10
+a = 4'b0010;
+b = 4'b1001;
+cin = 1;
+a = 4'b0110;
+b = 4'b1100;
+#10
+a = 4'b1110;
+b = 4'b1000;
+#10
+a = 4'b0111;
+b = 4'b1110;
+#10
+a = 4'b0010;
+b = 4'b1001;
+#10
+$finish();
+end
+
+endmodule
+
+/*
+
+
